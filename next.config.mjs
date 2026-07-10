e57c1287cca2c385481cf742879851a6724d7df4
Kryštof Ježek <krystof@kodeni.com>
2026-07-10 15:52:33 +0200

    Upgrade dependencies and clear audit

--- next.config.mjs
+++ next.config.mjs
@@ -21,12 +21,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/_next/static/(.*)',
-        headers: [
-          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
-        ],
-      },
-      {
         source: '/fonts/(.*)',
         headers: [
           { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
